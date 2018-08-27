ea31b1e29d92f1f7189815fa171b71b5a9876206
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-08-27 19:46:07 +0900

    'TIT_PC_complete'

--- test_py.py
+++ test_py.py
@@ -1,9 +1,10 @@
 #!python3.6
 import sys
 import matlab.engine
+
 def generate_plant_response() :
     eng = matlab.engine.start_matlab()
-
-    y = eng.generate_plant_response();
+-
+    y = eng.generate_plant_response()
     eng.quit()
     return y
